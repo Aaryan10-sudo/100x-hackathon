78868c9813d2da80b953284e40af8708ddc585e1
Aaryan10-sudo <lazyfox916@gmail.com>
2025-10-31 23:59:21 +0545

    pull

---
+++ client/src/app/(main)/explore/page.jsx
@@ -0,0 +1,12 @@
+import HeroSection from "@/components/explore/HeroSection";
+import React from "react";
+
+const page = () => {
+  return (
+    <section>
+      <HeroSection />
+    </section>
+  );
+};
+
+export default page;
