3d86eabdf3a7cb132133aaf879b2c543d4ea069d
Alish-Shakya <alishshakya44@gmail.com>
2025-11-01 08:39:58 +0545

    fixed code

--- client/src/app/(main)/layout.jsx
+++ client/src/app/(main)/layout.jsx
@@ -1,18 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import Providers from "./Providers";
 import Navbar from "@/layout/Navbar";
 import Footer from "@/layout/Footer";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
 
 export const metadata = {
   title: "Tourism Nepal || Your trip start here with us  ",
